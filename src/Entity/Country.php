70c2c1a1985e30ad9cc15b9b3f8c7eb1e3e9cb57
Tac Tacelosky <tacman@gmail.com>
2022-07-07 10:26:26 -0400

    update to 2.7, add heroku

--- src/Entity/Country.php
+++ src/Entity/Country.php
@@ -2,51 +2,47 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Put;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\CountryRepository;
 use Doctrine\ORM\Mapping as ORM;
-
-#[ORM\Entity(repositoryClass: CountryRepository::class)]
 #[ApiResource]
+#[ORM\Entity(repositoryClass: CountryRepository::class)]
 class Country
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
     private $id;
-
     #[ORM\Column(type: 'string', length: 255)]
     private $name;
-
     #[ORM\Column(type: 'string', length: 2)]
     private $alpha2;
-
-    public function getId(): ?int
+    public function getId() : ?int
     {
         return $this->id;
     }
-
-    public function getName(): ?string
+    public function getName() : ?string
     {
         return $this->name;
     }
-
-    public function setName(string $name): self
+    public function setName(string $name) : self
     {
         $this->name = $name;
-
         return $this;
     }
-
-    public function getAlpha2(): ?string
+    public function getAlpha2() : ?string
     {
         return $this->alpha2;
     }
-
-    public function setAlpha2(string $alpha2): self
+    public function setAlpha2(string $alpha2) : self
     {
         $this->alpha2 = $alpha2;
-
         return $this;
     }
 }
--- src/Entity/Official.php
+++ src/Entity/Official.php
@@ -2,128 +2,106 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Put;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\OfficialRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-
-#[ORM\Entity(repositoryClass: OfficialRepository::class)]
 #[ApiResource]
+#[ORM\Entity(repositoryClass: OfficialRepository::class)]
 class Official
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
     private $id;
-
     #[ORM\Column(type: 'string', length: 16, nullable: true)]
     private $firstName;
-
     #[ORM\Column(type: 'string', length: 32)]
     private $lastName;
-
     #[ORM\Column(type: 'string', length: 48)]
     private $officialName;
-
     #[ORM\Column(type: 'date_immutable', nullable: true)]
     private $birthday;
-
     #[ORM\Column(type: 'string', length: 1, nullable: true)]
     private $gender;
-
     #[ORM\OneToMany(mappedBy: 'offical', targetEntity: Term::class, orphanRemoval: true)]
     private $terms;
-
     public function __construct()
     {
         $this->terms = new ArrayCollection();
     }
-
-    public function getId(): ?int
+    public function getId() : ?int
     {
         return $this->id;
     }
-
-    public function getFirstName(): ?string
+    public function getFirstName() : ?string
     {
         return $this->firstName;
     }
-
-    public function setFirstName(?string $firstName): self
+    public function setFirstName(?string $firstName) : self
     {
         $this->firstName = $firstName;
-
         return $this;
     }
-
-    public function getLastName(): ?string
+    public function getLastName() : ?string
     {
         return $this->lastName;
     }
-
-    public function setLastName(string $lastName): self
+    public function setLastName(string $lastName) : self
     {
         $this->lastName = $lastName;
-
         return $this;
     }
-
-    public function getOfficialName(): ?string
+    public function getOfficialName() : ?string
     {
         return $this->officialName;
     }
-
-    public function setOfficialName(string $officialName): self
+    public function setOfficialName(string $officialName) : self
     {
         $this->officialName = $officialName;
-
         return $this;
     }
-
-    public function getBirthday(): ?\DateTimeImmutable
+    public function getBirthday() : ?\DateTimeImmutable
     {
         return $this->birthday;
     }
-
-    public function setBirthday(?\DateTimeImmutable $birthday): self
+    public function setBirthday(?\DateTimeImmutable $birthday) : self
     {
         $this->birthday = $birthday;
-
         return $this;
     }
-
-    public function getGender(): ?string
+    public function getGender() : ?string
     {
         return $this->gender;
     }
-
-    public function setGender(?string $gender): self
+    public function setGender(?string $gender) : self
     {
         $this->gender = $gender;
-
         return $this;
     }
-
     /**
      * @return Collection<int, Term>
      */
-    public function getTerms(): Collection
+    public function getTerms() : Collection
     {
         return $this->terms;
     }
-
-    public function addTerm(Term $term): self
+    public function addTerm(Term $term) : self
     {
         if (!$this->terms->contains($term)) {
             $this->terms[] = $term;
             $term->setOffical($this);
         }
-
         return $this;
     }
-
-    public function removeTerm(Term $term): self
+    public function removeTerm(Term $term) : self
     {
         if ($this->terms->removeElement($term)) {
             // set the owning side to null (unless already changed)
@@ -131,7 +109,6 @@ class Official
                 $term->setOffical(null);
             }
         }
-
         return $this;
     }
 }
